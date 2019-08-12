68687ff068a752a6b95711588b6ea91e29e738cf
Jesus gonzalez <sealeas27@gmail.com>
2019-08-11 22:21:47 -0400

    media queries for dashboard

--- frontend/src/components/Building.js
+++ frontend/src/components/Building.js
@@ -2,6 +2,7 @@ import React,{useEffect, useState} from 'react';
 import UserIcon from './icon-components/UserIcon';
 import EmailIcon from './icon-components/EmailIcon';
 import PhoneIcon from './icon-components/PhoneIcon';
+import './Cards.css';
 import {Link} from 'react-router-dom';
 
 const Building = ({id,image, price,name,email,phone, neighborhood, street, number, zip}) => {
@@ -11,17 +12,18 @@ const Building = ({id,image, price,name,email,phone, neighborhood, street, numbe
 					<img className="card__image" src={image} alt="{price}" />
 					<div className="card__body">
 						<h1 className="card__title">{price}</h1>
+						<ul className="card__adress-list">
+							<li className="card__paragraph"><strong>Colonia:</strong> {neighborhood}</li>
+							<li className="card__paragraph"><strong>Calle:</strong> {street}</li>
+							<li className="card__paragraph"><strong>Numero:</strong> {number}</li>
+							<li className="card__paragraph"><strong>Código postal:</strong> {zip}</li>
+						</ul>
 						<ul className="card__list-info">
 							<li className="card__li"><UserIcon />{name}</li>
 							<li className="card__li"><EmailIcon />{email}</li>
 							<li className="card__li"><PhoneIcon />{phone}</li>
 						</ul>
-						<div className="card__adress">
-							<p className="">Colonia: {neighborhood}</p>
-							<p className="">Calle: {street}</p>
-							<p className="">Numero: {number}</p>
-							<p className="">Código postal: {zip}</p>
-						</div>
+						
 						
 						<button className="card__button">editar</button>
 					</div>		
